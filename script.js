3117a8669e437d8950df66804994436c9300a434
Ngoc Anh Vu-Martell <anh.vumartell@github.io>
2021-03-30 21:50:05 +0300

    add button functionality

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 'use strict'
+//Select DOM elements
+const btnNext = document.querySelector('.btn-next'); 
+const btnPrev = document.querySelector('.btn-prev'); 
+
+const slides = document.querySelectorAll('.slide'); 
+let curSlide = 0;
+const maxSlide = slides.length; 
+
+
+//Create functions
+
+const goToSlide = function(slide){
+    slides.forEach((s,i) => s.style.transform =`translateX(${100*(i-slide)}%)`); 
+}
+
+const nextSlide = function(){
+    if(curSlide === maxSlide-1){
+        curSlide = 0;
+    } else{
+        curSlide++;
+    }
+    goToSlide(curSlide);
+
+}
+const prevSlide = function(){
+    if(curSlide === 0){
+        curSlide = maxSlide -1; 
+    } else {
+        curSlide--;
+    }
+    goToSlide(curSlide);
+}
+
+const init = function(){
+    goToSlide(0);
+}
+
+//Call init when page load
+init();
+//Add event listeners to each selected button
+btnNext.addEventListener('click', nextSlide);
+btnPrev.addEventListener('click', prevSlide); 
+
