9c0281d5002d2a056622744ceea3c4aafdd36782
Ngoc Anh Vu-Martell <anh.vumartell@github.io>
2021-03-31 20:08:15 +0300

    mobile view completed

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ const prevSlide = function(){
     }
     goToSlide(curSlide);
 }
+goToSlide(0);
+// const init = function(){
+//     goToSlide(0);
+// }
 
-const init = function(){
-    goToSlide(0);
-}
-
-//Call init when page load
-init();
+// //Call init when page load
+// init();
 //Add event listeners to each selected button
 btnNext.addEventListener('click', nextSlide);
 btnPrev.addEventListener('click', prevSlide); 
