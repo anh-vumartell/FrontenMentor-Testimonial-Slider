4bc04627b772f6f5a26994af16af742f83a6483f
Ngoc Anh Vu-Martell <anh.vumartell@github.io>
2021-04-06 19:42:48 +0300

    arrow keys function

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-'use strict'
+"use strict";
 //Select DOM elements
-const btnNext = document.querySelector('.btn-next'); 
-const btnPrev = document.querySelector('.btn-prev'); 
+const btnNext = document.querySelector(".btn-next");
+const btnPrev = document.querySelector(".btn-prev");
 
-const slides = document.querySelectorAll('.slide'); 
+const slides = document.querySelectorAll(".slide");
 let curSlide = 0;
-const maxSlide = slides.length; 
-
+const maxSlide = slides.length;
 
 //Create functions
 
-const goToSlide = function(slide){
-    slides.forEach((s,i) => s.style.transform =`translateX(${100*(i-slide)}%)`); 
-}
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
 
-const nextSlide = function(){
-    if(curSlide === maxSlide-1){
-        curSlide = 0;
-    } else{
-        curSlide++;
-    }
-    goToSlide(curSlide);
-
-}
-const prevSlide = function(){
-    if(curSlide === 0){
-        curSlide = maxSlide -1; 
-    } else {
-        curSlide--;
-    }
-    goToSlide(curSlide);
-}
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
 goToSlide(0);
 // const init = function(){
 //     goToSlide(0);
@@ -39,6 +39,10 @@ goToSlide(0);
 // //Call init when page load
 // init();
 //Add event listeners to each selected button
-btnNext.addEventListener('click', nextSlide);
-btnPrev.addEventListener('click', prevSlide); 
-
+btnNext.addEventListener("click", nextSlide);
+btnPrev.addEventListener("click", prevSlide);
+//Add event listeners to keyboard arrows
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") nextSlide();
+  if (e.key === "ArrowLeft") prevSlide();
+});
